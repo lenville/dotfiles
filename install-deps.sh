237be4e82764140f79d1ce1fb83327611f46e18a
lenville <lenville@gmail.com>
2015-01-04 21:47:36 +0800

    Add install-deps.sh from both Paul and Addy's

---
+++ install-deps.sh
@@ -0,0 +1,38 @@
+# up to you (me) if you want to run this as a file or copy paste at your leisure
+
+
+# https://github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+
+# https://rvm.io
+# rvm for the rubiess
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+# https://github.com/isaacs/nave
+# needs npm, obviously.
+# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
+npm install -g nave
+
+
+# homebrew!
+# you need the code CLI tools YOU FOOL.
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
+# google machines are funny so i have to do this. everyone else should use the regular thang
+mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
+export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
+
+
+# https://github.com/rupa/z
+# z, oh how i love you
+cd ~/code
+git clone https://github.com/rupa/z.git
+chmod +x ~/code/z/z.sh
+# also consider moving over your current .z file if possible. it's painful to rebuild :)
+
+# z binary is already referenced from .bash_profile
+
+
+# commonly used npm deps
+sudo npm install -g yo gulp grunt-cli jshint browserify generator-webapp generator-gulp-webapp
